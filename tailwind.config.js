6a01ef8f85a9d6317b91f41642677b9744b8bdea
ash9839 <sharma.ashish4979@gmail.com>
2022-10-13 18:13:38 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme:{
+    colors:{
+      treegreen : '#84CC16',
+    },
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
